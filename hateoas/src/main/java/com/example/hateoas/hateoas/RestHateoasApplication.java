e36d21111b5736e30c88cc456992c2bc492aac18
Orlando C. Patriarcha <orlando.patriarcha@gmail.com>
2024-08-12 02:29:49 -0300

    First commit

---
+++ hateoas/src/main/java/com/example/hateoas/hateoas/RestHateoasApplication.java
@@ -0,0 +1,13 @@
+package com.example.hateoas.hateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestHateoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestHateoasApplication.class, args);
+	}
+
+}
